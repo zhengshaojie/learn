3482694c3440c67a2ff2a7d6f7a2ec8c2d7c2a0c
郑少杰 <zhengshaojie@zhengshaojiedeMacBook-Pro.local>
2019-04-11 14:26:56 +0800

    bind event

--- react/hello-react/src/index.js
+++ react/hello-react/src/index.js
@@ -4,7 +4,60 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+class Title extends React.Component{
+
+    handClickOnTitle(){
+        alert('title is click')
+    }
+
+    render() {
+
+        return <h1 onClick={this.handClickOnTitle}>小书</h1>
+    }
+}
+
+class Header extends React.Component{
+
+    // renderGoodWord(goodWord, badWord) {
+    //     const isGoodWord = true
+    //     return isGoodWord ? goodWord : badWord
+    //
+    // }
+
+    render() {
+        // const className = 'test'
+        return <div>
+            <Title/>
+            <h2>this is header</h2>
+        </div>
+    }
+}
+class Main extends React.Component{
+
+        render() {
+            return <div>
+                <h2>this is main</h2>
+            </div>
+        }
+}
+class Footer extends React.Component{
+        render() {
+            return <div>
+                <h2>this is footer</h2>
+            </div>
+        }
+}
+class Index extends React.Component{
+        render() {
+            return <div>
+                <Header/>
+                <Main/>
+                <Footer/>
+            </div>
+        }
+}
+ReactDOM.render(<Index />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
